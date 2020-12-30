e715de935020fa2e750a27ea563a3a696f69927c
Santiago Guerra <santiagoalejoguerra@gmail.com>
2020-12-30 17:48:07 -0300

    Adding auth to our Serverless API

--- src/update.js
+++ src/update.js
@@ -8,7 +8,7 @@ export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
         Key: {
-            userId: "123",
+            userId: event.requestContext.identity.cognitoIdentityId,
             noteId: event.pathParameters.id,
         },
 
